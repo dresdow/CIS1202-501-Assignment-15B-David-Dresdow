eec32a02c6824b31f1b2130fee86441d6417758a
dunitas <92960353+dunitas@users.noreply.github.com>
2021-12-05 16:15:18 -0500

    Create comments and include stmts needed for program

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+/*********************************************************************************************************************************/
+/*  David Dresdow                                                                                                                */
+/*                                                                                                                               */
+/*  CIS 1202 501                                                                                                                 */
+/*                                                                                                                               */
+/*  December , 2021                                                                                                              */
+/*                                                                                                                               */
+/*  Program Name - Programming Assignment 15b  - Template Functions                                                              */
+/*                                                                                                                               */
+/* This program will calculate what half of a number is using a function called half                                             */
+/*   1) one generic template function will handle both double and float number values                                            */
+/*   2) one explicit template function will handle integer values                                                                */
+/*                                                                                                                               */
+/********************************************************************************************************************************/
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cctype>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
